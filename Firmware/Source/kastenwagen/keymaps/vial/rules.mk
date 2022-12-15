fe53fa9132e2dfdf4cf19c92f25029383606be61
Technofrikus <tf@MBAM2.fritz.box>
2022-12-15 14:21:11 +0100

    Added Firmware

---
+++ Firmware/Source/kastenwagen/keymaps/vial/rules.mk
@@ -0,0 +1,8 @@
+VIA_ENABLE = yes            # VIA support
+VIAL_ENABLE = yes            # VIAL support
+LTO_ENABLE = yes            #
+ENCODER_MAP_ENABLE = yes
+QMK_SETTINGS = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
